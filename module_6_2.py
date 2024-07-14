c73a214eca28640c6cd0b51c85ebf290133bbd4c
KakTys234 <168638151+KakTys234@users.noreply.github.com>
2024-07-14 15:19:27 +0300

    Add files via upload

---
+++ module_6_2.py
@@ -0,0 +1,29 @@
+class Vehicle:
+
+    def owner(str):
+
+
+    def __model(str):
+
+
+    def __engine_power(int):
+
+
+    def __color(str):
+
+
+__COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+
+
+class Sedan(Vehicle):
+
+    vehicle1.print_info()
+
+
+    vehicle1.set_color('Pink')
+    vehicle1.set_color('BLACK')
+    vehicle1.owner = 'Vasyok'
+
+    
+    vehicle1.print_info()
